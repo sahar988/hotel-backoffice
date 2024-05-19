10480d7763e6b50259157a899c40e274a059031c
sahar <s.ghasemee@gmail.com>
2024-05-19 13:18:00 -0700

    revoke key

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabaseUrl = "https://nmblnsujkpscyusdevif.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5tYmxuc3Vqa3BzY3l1c2RldmlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDAzMzgzODAsImV4cCI6MjAxNTkxNDM4MH0.kdDkf_KBoZz9qtxNOuQQe2GMpsizsWKSfhmd9uUrCd0";
+const supabaseKey = "";
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
